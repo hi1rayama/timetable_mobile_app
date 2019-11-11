72bdb3cd845393ace1507d1933dc950a85889b2c
平山快 <>
2019-11-11 21:38:22 +0900

    Create  a setting menu screen

---
+++ src/conmponents/SettingMenu.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import {StyleSheet,View,Text} from 'react-native';
+
+
+class SettingMenu extends React.Component{
+    render(){
+        return(
+            <View style={styles.settingList} >
+                     <View style={styles.settingMenuItem}>
+                        <Text style={styles.settingTitle}>時間割の設定</Text>
+                    </View>
+
+                    <View style={styles.settingMenuItem}>
+                        <Text style={styles.settingTitle}>現状の設定</Text>
+                    </View>
+
+                    <View style={styles.settingMenuItem}>
+                        <Text style={styles.settingTitle}>タグの追加</Text>
+                    </View>
+            </View>
+
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    settingList:{
+        flexDirection:'column',
+        paddingTop:90,
+
+    },
+      settingMenuItem:{
+        width:'100%',
+        padding:16,
+        borderBottomWidth:1,
+        borderBottomColor:'#ddd',
+      },
+      settingTitle:{
+          width:'100%',
+        fontSize:30,
+        marginBottom: 4,
+    
+      },
+
+})
+
+
+
+
+export default SettingMenu;
---
+++ src/screens/SettingHomeScreen.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import {StyleSheet,View,Text} from 'react-native';
+
+import Bar from '../elements/Bar';
+import SettingMenu from '../conmponents/SettingMenu';
+
+
+class SettingHomeScreen extends React.Component{
+    render(){
+        return(
+            
+                <View style={styles.container}  >
+                    <View >
+                    <Bar name='設  定'/>
+                    </View>
+
+                    <View style={styles.settingMenu}>
+                    <SettingMenu />
+                    </View>
+                    
+                </View>
+                
+           
+            
+
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      width:'100%',
+     
+    },
+    settingMenu:{
+    
+      },
+})
+export default SettingHomeScreen;
