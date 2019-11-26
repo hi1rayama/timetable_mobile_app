14f310a0880e11ec158134dcf4797d0eda1f1fe8
平山快 <hi1rayama@gmail.com>
2019-11-27 00:07:17 +0900

    firebaseから講義名の取得

--- src/screens/TodoDetailScreen.js
+++ src/screens/TodoDetailScreen.js
@@ -6,35 +6,54 @@ import firebase from 'firebase';
 
 class TodoDetailScreen extends React.Component {
     state = {
-        item:{},
-        nameList:{},
+        item: {},
+        nameList: {},
+        name: ''
     }
     componentWillMount() {
         const { params } = this.props.navigation.state;
         this.setState({ item: params.item });
+        const db = firebase.firestore();
+        console.log(params.item.name_key);
+        const { currentUser } = firebase.auth();
+        db.collection(`/users/${currentUser.uid}/${params.item.class}`).doc(params.item.name_key)
+            .get()
+            .then((doc) => {
+                console.log(doc.data());
+                this.setState({ name: doc.data().name });
+                console.log(this.state.name);
+
+
+            })
+            .catch((error) => {
+                console.log(error);
+            })
+
+
 
     }
-    
+
     render() {
         const { item } = this.state;
         return (
             <View style={styles.container}>
-                <View >
-
-                    <View style={styles.todoHeader}>
-                        <View>
-                            <Text style={styles.todoHeaderTitle}>{item.title}</Text>
-                            <Text style={styles.todoHeaderDeadline}>{item.deadline}</Text>
-                        </View>
-                    </View>
+                <View style={styles.inline}>
+                    <Text style={styles.textformat}>講義名: </Text>
+        <Text style={styles.inputformat}>{this.state.name}</Text>
                 </View>
 
+                <View style={{ flexDirection: 'row' }}>
+                    <Text style={styles.textformat}>タイトル: </Text>
+                    <Text style={styles.inputformat}>{item.title}</Text>
+                </View>
+
+                <View style={{ flexDirection: 'row' }}>
+                    <Text style={styles.textformat}>期限　: </Text>
+                    <Text style={styles.inputformat}>{item.deadline}</Text>
+                </View>
                 <View style={styles.todoContent}>
-                <Text style={styles.todobody}>
-                        詳細：
-                    </Text>
-                    <Text style={styles.todobody}>
-                        {item.content}
+                    <Text style={styles.todoBody}>
+                       内容 {item.content}
                     </Text>
                 </View>
             </View>
@@ -47,27 +66,36 @@ class TodoDetailScreen extends React.Component {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        width: '100%'
+        width: '100%',
+        padding: 24
+
+    }, inline: {
+        justifyContent: 'space-evenly',
+        flexDirection: 'row',
+        paddingTop: 15,
+
 
     },
-    todoHeader: {
-        height: 100,
-        backgroundColor: '#17313C',
-        justifyContent: 'center',
-        padding: 10,
+    textformat: {
+        fontSize: 30,
+
 
     },
-    todoHeaderTitle: {
+    inputformat: {
+        borderRadius: 15,
+        backgroundColor: '#eee',
+        height: 48,
+        marginBottom: 16,
+        borderColor: '#DDD',
+        padding: 8,
+        width: '70%',
         fontSize: 20,
-        fontWeight: 'bold',
-        color: '#fff',
 
     },
-    todoHeaderDeadline: {
-        fontSize: 12,
-        color: '#fff'
-
-    },
+    todoBody:{
+        lineHeight:20,
+        fontSize:15,
+    }, 
     todoContent: {
         paddingTop: 30,
         paddingLeft: 20,
@@ -76,10 +104,6 @@ const styles = StyleSheet.create({
         flex: 1,
 
     },
-    todobody:{
-        lineHeight:20,
-        fontSize:15,
-    }
 
 });
 
